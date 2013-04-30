e9c999e1f737b97900df6af8efa22f1a292870e0
sandcountyfrank@gmail.com <sandcountyfrank@gmail.com@0eb2049b-780d-fcd2-2b23-8f93affdebe1>
2013-04-30 14:56:29 +0000

    Tag of VMTv4.0br2

---
+++ tags/VMTv4.0br2/utils/disableMenuBar.m
@@ -0,0 +1,22 @@
+function disableMenuBar(hf)
+% Customizes the figure toolbars. Input argument is the figure handle (hf)
+
+all_tools = findall(hf); % finds hidden tools
+hide_tools = findall(all_tools,'ToolTipString','Show Plot Tools and Dock Figure'); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','Hide Plot Tools')); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','Open File')); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','New Figure')); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','Insert Legend')); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','Insert Colorbar')); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','Data Cursor')); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','Rotate 3D')); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','Save')); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','Brush/Select Data')); 
+hide_tools = vertcat(hide_tools,findall(all_tools,'ToolTipString','Link Plot')); 
+set(hide_tools,'Visible','Off')
+
+hide_menu = findall(all_tools,'tag','figMenuDesktop');
+hide_menu = vertcat(hide_menu,findall(all_tools,'tag','figMenuWindow'));
+hide_menu = vertcat(hide_menu,findall(all_tools,'tag','figMenuTools'));
+hide_menu = vertcat(hide_menu,findall(all_tools,'tag','figMenuInsert'));
+set(hide_menu,'visible','off')
